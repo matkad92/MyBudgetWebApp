5e10c9169cfe329410ce5ebe44358c8a454beebe
Mateusz Kadziolka <mateuszkadziolka.programista@gmail.com>
2021-10-25 14:02:59 +0200

    Balance Page redesigned

---
+++ app.js
@@ -0,0 +1,6 @@
+var myModal = new bootstrap.Modal(document.getElementById("exampleModal"), {});
+
+document.getElementById("toggleModal").addEventListener('click',function ()
+{
+     myModal.show(); 
+}  ); 
